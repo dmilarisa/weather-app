39ddc9beeb95399809e7eb750da9eb56a5389d77
Lara <dmilarisa@gmail.com>
2022-07-16 10:24:50 +0100

    updated date

--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`
+  };
+  
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  };
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",];
+  let day = days[date.getDay()];
+
+  return `Updated: ${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+  
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
+  
   let precipitaionElement = document.querySelector("#humidity");
   precipitaionElement.innerHTML = response.data.main;
+  
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
+  
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
 
   console.log(response.data);
